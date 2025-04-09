43ad24305d8788d470d9dee9d68194d5318cacc3
Eldiyar <eldiyar1mic2soft3@gmail.com>
2025-04-09 16:33:14 +0600

    Исправил ошибки, которые были в 'exit'

--- online_shop_project/src/application/command/exit.java
+++
@@ -1,9 +0,0 @@
-package application.command;
-import application.model.*;
-public class exit implements Command {
-    @Override
-    public void execute() {System.exit(0);}
-
-    @Override
-    public String toString() {return "exit - завершилась работа программы";}
-}
